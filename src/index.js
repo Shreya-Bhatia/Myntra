097be29d24736e2536549db7765775f89fc3d334
Shreya Bhatia <shreyakk17@gmail.com>
2024-07-11 19:05:44 +0530

    Added the basic route and basic ui of signin page

--- src/index.js
+++ src/index.js
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import SignIn from './pages/SignIn/signin';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/signin",
+    element: <SignIn/>,
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ src/pages/SignIn/signin.js
@@ -0,0 +1,14 @@
+import './signin.css'
+
+function SignIn() {
+	return (
+		<div className="signin">
+			<div className="maintext">
+			Let the <span style={{'-webkit-text-stroke' : '1px deeppink', 'color': 'transparent'}}>FASHION</span> journey begin !!
+			</div>
+			<button className='signinbutton'>Sign In To Shop Now</button>
+		</div>
+	);
+}
+
+export default SignIn;
