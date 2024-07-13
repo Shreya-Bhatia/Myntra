75fcf09786f5928bbf9bd1caae9aeecf5fc3505a
Shreya Bhatia <shreyakk17@gmail.com>
2024-07-13 15:20:32 +0530

    Added uploading ootd part

--- src/pages/LeaderBoard/Lead.js
+++ src/pages/LeaderBoard/Lead.js
@@ -4,14 +4,63 @@ import './Lead.css'
 import React, { useRef} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons'; 
+import {db} from '../../firebase'
+import { doc, setDoc } from "firebase/firestore";
+import { useState } from "react";
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
+import { getAuth } from 'firebase/auth';
 
 function Lead() {
-const fileInput=useRef(null);
-const handleButtonClick=()=>{
-    fileInput.current.click();
-};
-    
 
+    const storage = getStorage();
+    const auth = getAuth();
+
+    // const fileInput=useRef(null);
+    
+    // const handleButtonClick=async ()=>{
+    //     await fileInput.current.click();
+    // };
+
+    const [file, setFile] = useState();
+    const [isLoading,setLoad] = useState(false);
+
+	function handleChange(event) {
+		setFile(event.target.files[0])
+	}
+    
+    async function submitOOTD(e) {
+
+		e.preventDefault();
+
+        const uid = auth.currentUser.uid;
+
+		if(file==null) {
+            return;
+        }
+
+        setLoad(true);
+
+		const imgref = ref(storage, 'ootd_images/' + uid);
+		await uploadBytes(imgref,file);
+		
+        const uootd = {
+            img: "",
+            users_liked: [],
+            no_of_likes: 0,
+            id: uid
+        };
+
+		await getDownloadURL(imgref).then((url) => {
+			uootd.img = url;
+		});
+
+		const OOTDRef = doc(db,"OOTD",uid);
+		await setDoc(OOTDRef, uootd);
+
+        setLoad(false);
+
+        alert('OOTD uploaded successfully...');
+	}
     
 	return (
 		<div className="Lead">
@@ -30,17 +79,24 @@ const handleButtonClick=()=>{
                 <div className="title-here">#fashionStreak</div>
                 <div className="sub">Share your everyday fashion to maintain your fashion streak and win exciting prizes!</div>
             </div>
-			<button className='ootd' onClick={handleButtonClick}>
-                    Upload Your #OOTD
-                    <input 
-                        type="file" 
-                        accept="image/*"
-                        ref={fileInput} 
-                        
-                        style={{ display: 'none' }} 
-                    />
-                </button>
-			
+            {
+                isLoading
+                ? <h3>Loading....</h3>
+                : <form onSubmit={submitOOTD}>
+                    <input type="file" accept="image/*" onChange={handleChange}/>
+                    <input type="submit" />
+			            {/* <button type='submit' className='ootd'>
+                                Upload Your #OOTD
+                                <input 
+                                    type="file" 
+                                    accept="image/*"
+                                    // ref={fileInput} 
+                                    onChange={handleChange}
+                                    // style={{ display: 'none' }} 
+                                />
+                        </button> */}
+			        </form>
+            }
 			<div></div></div>
             <div className='others'><p>See what others are wearing today</p><FontAwesomeIcon icon={faAngleDown} style={{marginTop:'1%' ,marginLeft:'1%'}}/></div>
             <div className="outfits"></div>
